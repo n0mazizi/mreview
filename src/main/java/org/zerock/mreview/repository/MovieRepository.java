e04a9bd89f1954fc177bf1aa632d628958af0b53
n0mazizi <heoyunhoe@gmail.com>
2023-03-06 17:58:28 +0900

    MovieRepository 페이지 처리되는 영화별 평균점수/리뷰개수 구하기 query 개선

--- src/main/java/org/zerock/mreview/repository/MovieRepository.java
+++ src/main/java/org/zerock/mreview/repository/MovieRepository.java
@@ -9,6 +9,7 @@ import org.zerock.mreview.entity.Movie;
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     @Query("select m, avg(coalesce(r.grade, 0)), count(distinct r) from Movie m " +
-            "left outer join Review r on r.movie=m group by m")
+            "left outer join MovieImage mi on mi.movie = m " +
+            "left outer join Review r on r.movie = m group by m")
     Page<Object[]> getListPage(Pageable pageable);
 }
