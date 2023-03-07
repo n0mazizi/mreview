32ba184ba1ea6feb2bae046aed77c9a2cb350747
n0mazizi <heoyunhoe@gmail.com>
2023-03-07 09:31:55 +0900

    특정 영화 조회 query TEST

--- src/test/java/org/zerock/mreview/repository/MovieRepositoryTest.java
+++ src/test/java/org/zerock/mreview/repository/MovieRepositoryTest.java
@@ -12,6 +12,7 @@ import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.MovieImage;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.IntStream;
@@ -62,6 +63,18 @@ class MovieRepositoryTest {
 
         for (Object[] objects : result.getContent()) {
             System.out.println(Arrays.toString(objects));
+        }
+    }
+
+
+    @Test
+    public void testGetMovieWithAll() {
+        List<Object[]> result = movieRepository.getMovieWithAll(94L);
+
+        System.out.println(result);
+
+        for (Object[] arr : result) {
+            System.out.println(Arrays.toString(arr));
         }
     }
 
