66ebc6fb466e5377b78a3ad5f7d28545fc3ca333
n0mazizi <heoyunhoe@gmail.com>
2023-03-07 09:36:48 +0900

    특정 영화 조회 query 수정(group by)

--- src/main/java/org/zerock/mreview/repository/MovieRepository.java
+++ src/main/java/org/zerock/mreview/repository/MovieRepository.java
@@ -16,8 +16,9 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     Page<Object[]> getListPage(Pageable pageable);  //페이지 처리
 
 
-    @Query("select m, mi from Movie m " +
+    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(r) from Movie m " +
             "left outer join MovieImage mi on mi.movie = m " +
-            "where m.mno = :mno")
+            "left outer join Review r on r.movie = m " +
+            "where m.mno = :mno group by mi")
     List<Object[]> getMovieWithAll(Long mno);  //특정 영화 조회
 }
