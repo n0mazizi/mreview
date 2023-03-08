e33dfaa9f5767930ac00af731f03197c1c0702ba
n0mazizi <heoyunhoe@gmail.com>
2023-03-08 15:43:03 +0900

    thumbnail image 보기

--- src/main/java/org/zerock/mreview/controller/UploadController.java
+++ src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -1,6 +1,7 @@
 package org.zerock.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -64,7 +65,18 @@ public class UploadController {
             Path savePath = Paths.get(saveName);
 
             try {
+                //원본 파일 저장
                 uploadFile.transferTo(savePath);
+
+                //Thumbnail 파일명 생성
+                String thumbnailSaveName = uploadPath + File.separator +
+                        folderPath + File.separator + "s_" + uuid + "_" + fileName;
+
+                File thumbnailFile = new File(thumbnailSaveName);
+
+                //Thumbnail 생성
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile, 100, 100);
+
                 resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch (IOException e) {
                 e.printStackTrace();
