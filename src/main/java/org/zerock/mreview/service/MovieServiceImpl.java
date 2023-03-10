52a447edf771ab5ebc2df0bbaae5d70eedb51c4d
n0mazizi <heoyunhoe@gmail.com>
2023-03-10 10:31:24 +0900

    3. 이미지 등록 및 삭제 service,  html 수정

---
+++ src/main/java/org/zerock/mreview/service/MovieServiceImpl.java
@@ -0,0 +1,42 @@
+package org.zerock.mreview.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.mreview.dto.MovieDTO;
+import org.zerock.mreview.entity.Movie;
+import org.zerock.mreview.entity.MovieImage;
+import org.zerock.mreview.repository.MovieImageRepository;
+import org.zerock.mreview.repository.MovieRepository;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class MovieServiceImpl implements MovieService {
+
+    //MovieRepository와 MovieImageRepository를 주입받아
+    //dtoToEntity()에서 반환한 객체들을 이용해서 save()를 처리
+
+    private final MovieRepository movieRepository;
+    private final MovieImageRepository imageRepository;
+
+    @Transactional
+    @Override
+    public Long register(MovieDTO movieDTO) {
+        Map<String, Object> entityMap = dtoToEntity(movieDTO);
+        Movie movie = (Movie) entityMap.get("movie");
+        List<MovieImage> movieImageList = (List<MovieImage>) entityMap.get("imgList");
+
+        movieRepository.save(movie);
+
+        movieImageList.forEach(movieImage -> {
+            imageRepository.save(movieImage);
+        });
+
+        return movie.getMno();
+    }
+}
